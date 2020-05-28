135736931a143ded2a2ca059e41f9f1e9f452ff4
Nick Rentschler <nickrench3@icloud.com>
2020-05-28 14:24:14 -0500

    Add project files.

---
+++ PaycheckCalculator/Paycheck.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace PaycheckCalculator
+{
+    partial class Paycheck
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.totalCheckBox = new System.Windows.Forms.TextBox();
+            this.wageTextBox = new System.Windows.Forms.TextBox();
+            this.hoursTextBox = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.EnterButton = new System.Windows.Forms.Button();
+            this.ClearButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // totalCheckBox
+            // 
+            this.totalCheckBox.Location = new System.Drawing.Point(230, 238);
+            this.totalCheckBox.Name = "totalCheckBox";
+            this.totalCheckBox.ReadOnly = true;
+            this.totalCheckBox.Size = new System.Drawing.Size(146, 20);
+            this.totalCheckBox.TabIndex = 0;
+            this.totalCheckBox.TabStop = false;
+            // 
+            // wageTextBox
+            // 
+            this.wageTextBox.Location = new System.Drawing.Point(230, 141);
+            this.wageTextBox.Name = "wageTextBox";
+            this.wageTextBox.Size = new System.Drawing.Size(146, 20);
+            this.wageTextBox.TabIndex = 2;
+            // 
+            // hoursTextBox
+            // 
+            this.hoursTextBox.Location = new System.Drawing.Point(230, 80);
+            this.hoursTextBox.Name = "hoursTextBox";
+            this.hoursTextBox.Size = new System.Drawing.Size(146, 20);
+            this.hoursTextBox.TabIndex = 1;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label4.Location = new System.Drawing.Point(31, 241);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(161, 17);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Approx. Paycheck Total:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label3.Location = new System.Drawing.Point(31, 141);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(132, 17);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Enter Hourly Wage:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label2.Location = new System.Drawing.Point(31, 80);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(141, 17);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Enter Hours Worked:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.label1.Location = new System.Drawing.Point(128, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(191, 25);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Paycheck Calculator";
+            // 
+            // EnterButton
+            // 
+            this.EnterButton.BackColor = System.Drawing.Color.Chartreuse;
+            this.EnterButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.EnterButton.Location = new System.Drawing.Point(244, 178);
+            this.EnterButton.Name = "EnterButton";
+            this.EnterButton.Size = new System.Drawing.Size(107, 33);
+            this.EnterButton.TabIndex = 3;
+            this.EnterButton.Text = "Enter";
+            this.EnterButton.UseVisualStyleBackColor = false;
+            this.EnterButton.Click += new System.EventHandler(this.EnterButton_Click);
+            // 
+            // ClearButton
+            // 
+            this.ClearButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.ClearButton.Location = new System.Drawing.Point(157, 295);
+            this.ClearButton.Name = "ClearButton";
+            this.ClearButton.Size = new System.Drawing.Size(107, 33);
+            this.ClearButton.TabIndex = 4;
+            this.ClearButton.Text = "Clear";
+            this.ClearButton.UseVisualStyleBackColor = true;
+            this.ClearButton.Click += new System.EventHandler(this.ClearButton_Click);
+            // 
+            // Paycheck
+            // 
+            this.AcceptButton = this.EnterButton;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(463, 353);
+            this.Controls.Add(this.ClearButton);
+            this.Controls.Add(this.EnterButton);
+            this.Controls.Add(this.totalCheckBox);
+            this.Controls.Add(this.wageTextBox);
+            this.Controls.Add(this.hoursTextBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Paycheck";
+            this.Text = "Paycheck Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox totalCheckBox;
+        private System.Windows.Forms.TextBox wageTextBox;
+        private System.Windows.Forms.TextBox hoursTextBox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button EnterButton;
+        private System.Windows.Forms.Button ClearButton;
+    }
+}
+
---
+++ PaycheckCalculator/Paycheck.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PaycheckCalculator
+{
+    public partial class Paycheck : Form
+    {
+        public Paycheck()
+        {
+            InitializeComponent();
+        }
+
+        private void EnterButton_Click(object sender, EventArgs e)
+        {
+            double hoursWorked = Convert.ToDouble(hoursTextBox.Text);
+            double wages = Convert.ToDouble(wageTextBox.Text);
+            double total = (hoursWorked * wages) * 0.77365;
+            total = Math.Round(total, 2);
+
+            totalCheckBox.Text = total.ToString();
+        }
+
+        private void ClearButton_Click(object sender, EventArgs e)
+        {
+            hoursTextBox.Clear();
+            wageTextBox.Clear();
+            totalCheckBox.Clear();
+        }
+    }
+}
